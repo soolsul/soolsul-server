57bc745eff5c29d475f411505ababa5e21abce5c
vcho1958 <gusckd57@gmail.com>
2022-11-02 05:32:31 +0900

    [fix] Authority 누락된 어노테이션 추가

--- src/main/java/com/soolsul/soolsulserver/domain/auth/Authority.java
+++ src/main/java/com/soolsul/soolsulserver/domain/auth/Authority.java
@@ -1,6 +1,9 @@
 package com.soolsul.soolsulserver.domain.auth;
 
 import com.soolsul.soolsulserver.domain.common.BaseEntity;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
 
 import javax.persistence.Entity;
@@ -9,6 +12,8 @@ import javax.persistence.Enumerated;
 
 
 @Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Authority extends BaseEntity implements GrantedAuthority {
     @Enumerated(value = EnumType.STRING)
     private Role authority;
