ba913cce78786485d4535bcdfc1ffb5f20524d4b
jiwoo <zbqmgldjfh@gmail.com>
2022-11-06 15:43:11 +0900

    feat: DatabaseCleanup 을 통한 테스트간 격리 구현
    DatabaseCleanup를 통하여 각 테스트 마다 DB상의 데이터를 직접 초기화 한다

---
+++ src/test/java/com/soolsul/soolsulserver/acceptance/AcceptanceTest.java
@@ -0,0 +1,30 @@
+package com.soolsul.soolsulserver.acceptance;
+
+import com.soolsul.soolsulserver.utils.DatabaseCleanup;
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.test.context.ActiveProfiles;
+
+@ActiveProfiles("test")
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class AcceptanceTest {
+
+    protected static final String ADMIN_EMAIL = "admin@email.com";
+    protected static final String ADMIN_PASSWORD = "password";
+    protected static final String NAME = "admin";
+
+    @LocalServerPort
+    int port;
+
+    @Autowired
+    private DatabaseCleanup databaseCleanup;
+
+    @BeforeEach
+    public void setUp() {
+        RestAssured.port = port;
+        databaseCleanup.execute();
+    }
+}
