b5384d4a547129a0d25da7079668c1fe0dcea3b6
cooper <pbg0205@naver.com>
2022-11-07 23:38:22 +0900

    test: localstack 을 이용한 AwsS3 업로드 로직 테스트
    - DockerImageName: 도케이미지를 호출하는 방법
    - deprecated 코드 추가 리팩토링 필요
        - localStackContainer.getEndpointConfiguration(S3)
        - localStackContainer.getDefaultCredentialsProvider()

---
+++ src/test/java/com/soolsul/soolsulserver/images/S3ImageHandlerTest.java
@@ -0,0 +1,72 @@
+package com.soolsul.soolsulserver.images;
+
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockMultipartFile;
+
+import java.io.IOException;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = LocalStackS3Config.class)
+class S3ImageHandlerTest {
+
+    @Value("${aws.s3.images.bucket.name}")
+    private String bucketName;
+
+    @Autowired
+    private AmazonS3 amazonS3;
+
+    @Autowired
+    private AwsS3ImageHandler awsS3ImageHandler;
+
+    @BeforeEach
+    void init() {
+        amazonS3.createBucket(bucketName);
+        amazonS3.putObject(bucketName, "restaurant/user01", "");
+    }
+
+    @DisplayName("이미지를 s3에 업로드 한다.")
+    @Test
+    void uploadImageFile() {
+        //given
+        MockMultipartFile multipartFile = new MockMultipartFile(
+                "multipartFile",
+                "test1.PNG",
+                MediaType.MULTIPART_FORM_DATA_VALUE,
+                "test1".getBytes());
+
+        //when
+        String uploadImageUrl = awsS3ImageHandler.uploadImage(multipartFile, "restaurant", "user01");
+
+        //then
+        assertThat(uploadImageUrl).isNotEmpty();
+    }
+
+    @DisplayName("이미지를 검색한다")
+    @Test
+    void findFileNames() throws IOException {
+        //given
+        String imageName01 = "test1.PNG";
+        String imageName02 = "test2.PNG";
+        MockMultipartFile multipartFile01 = new MockMultipartFile("multipartFile", imageName01, MediaType.MULTIPART_FORM_DATA_VALUE, "test1".getBytes());
+        MockMultipartFile multipartFile02 = new MockMultipartFile("multipartFile", imageName02, MediaType.MULTIPART_FORM_DATA_VALUE, "test1".getBytes());
+
+        amazonS3.putObject(bucketName, "restaurant/user01/" + imageName01, multipartFile01.getInputStream(), new ObjectMetadata());
+        amazonS3.putObject(bucketName, "restaurant/user01/" + imageName02, multipartFile02.getInputStream(), new ObjectMetadata());
+
+        //when
+        List<String> fileNames = awsS3ImageHandler.findFileNames("restaurant", "user01");
+
+        //then
+        assertThat(fileNames).hasSize(2);
+    }
+}
