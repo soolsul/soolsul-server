3b1648e0e027b7fbb3d15b69b0deb12a30226298
jiwoo <zbqmgldjfh@gmail.com>
2022-11-08 14:43:16 +0900

    feat: Post, Restaurant Repository 구현
    단순한 find, save 용도라 Data JPA 사용

---
+++ src/main/java/com/soolsul/soolsulserver/restaurant/RestaurantRepository.java
@@ -0,0 +1,6 @@
+package com.soolsul.soolsulserver.restaurant;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RestaurantRepository extends JpaRepository<Restaurant, String> {
+}
