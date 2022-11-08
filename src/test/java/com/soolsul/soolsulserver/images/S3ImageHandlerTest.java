31f3bccc6e3c5e420b6d2404ed2eb546ff9f7d2d
cooper <pbg0205@naver.com>
2022-11-08 21:42:25 +0900

    test: 이미지 파일 미존재 또는 이름이 미존재한 경우의 테스트 케이스 추가

--- src/test/java/com/soolsul/soolsulserver/images/S3ImageHandlerTest.java
+++ src/test/java/com/soolsul/soolsulserver/images/S3ImageHandlerTest.java
@@ -5,16 +5,25 @@ import com.amazonaws.services.s3.model.ObjectMetadata;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.NullSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.web.multipart.MultipartException;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+import static org.junit.jupiter.params.provider.Arguments.*;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = LocalStackS3Config.class)
 class S3ImageHandlerTest {
@@ -49,6 +58,34 @@ class S3ImageHandlerTest {
 
         //then
         assertThat(uploadImageUrl).isNotEmpty();
+    }
+
+    @DisplayName("이미지 파일이 존재하지 않는다면 예외를 반환한다.")
+    @Test
+    void throwExceptionWhenImageNameIsEmpty() {
+        //given, when, then
+        assertThatExceptionOfType(MultipartException.class)
+                .isThrownBy(() -> awsS3ImageHandler.uploadImage(null, "restaurant", "user01"));
+    }
+
+    public static Stream<String> originalEmptyNameProvider() {
+        return Stream.of(null, "");
+    }
+
+    @DisplayName("이미지 파일 이름이 존재하지 않는다면 예외를 반환한다.")
+    @MethodSource("originalEmptyNameProvider")
+    @ParameterizedTest
+    void throwExceptionWhenImageNameIsEmpty(String originalFileName) {
+        //given
+        MockMultipartFile multipartFile = new MockMultipartFile(
+                "multipartFile",
+                originalFileName,
+                MediaType.MULTIPART_FORM_DATA_VALUE,
+                "test1".getBytes());
+
+        //when, then
+        assertThatExceptionOfType(MultipartException.class)
+                .isThrownBy(() -> awsS3ImageHandler.uploadImage(multipartFile, "restaurant", "user01"));
     }
 
     @DisplayName("이미지를 검색한다")
