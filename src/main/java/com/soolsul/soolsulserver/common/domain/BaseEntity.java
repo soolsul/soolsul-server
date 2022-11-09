02863b36212a4db153e5e793112cc0ec6249717b
jiwoo <zbqmgldjfh@gmail.com>
2022-11-09 14:43:31 +0900

    refactor: BaseEntity 자체에 equals&hashcode 구현

--- src/main/java/com/soolsul/soolsulserver/common/domain/BaseEntity.java
+++ src/main/java/com/soolsul/soolsulserver/common/domain/BaseEntity.java
@@ -8,6 +8,7 @@ import org.hibernate.annotations.GenericGenerator;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
+import java.util.Objects;
 
 @MappedSuperclass
 @Getter
@@ -20,5 +21,18 @@ public abstract class BaseEntity {
 
     protected BaseEntity(String id) {
         this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        BaseEntity that = (BaseEntity) o;
+        return Objects.equals(getId(), that.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
     }
 }
