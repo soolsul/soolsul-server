ef5dc41175ffc7df3bc52a94dccf6aae99cde344
jiwoo <zbqmgldjfh@gmail.com>
2022-11-11 21:24:24 +0900

    style: PostDetailResponse을 record타입으로 변경

--- src/main/java/com/soolsul/soolsulserver/post/presentation/dto/PostDetailResponse.java
+++ src/main/java/com/soolsul/soolsulserver/post/presentation/dto/PostDetailResponse.java
@@ -1,23 +1,44 @@
 package com.soolsul.soolsulserver.post.presentation.dto;
 
+import com.soolsul.soolsulserver.post.business.dto.PostDetailLikeResponse;
 import com.soolsul.soolsulserver.post.domain.Post;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.URL;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 import java.util.List;
+import java.util.Objects;
 
-@Getter
-@NoArgsConstructor
-@AllArgsConstructor
-public class PostDetailResponse {
 
-    private String UserName;
-    private Float Score;
-    private List<String> ImageUrls;
-    private String Contents;
+public record PostDetailResponse(
 
-    public static PostDetailResponse of(String userName, Post post, List<String> urlList) {
-        return new PostDetailResponse(userName, post.getScore(), urlList, post.getContents());
+        @NotNull
+        String UserName,
+
+        @NotNull
+        @Pattern(regexp = "1-5")
+        Float Score,
+
+        @NotBlank
+        String Contents,
+
+        @URL
+        @Size(max = 10)
+        List<String> ImageUrls,
+
+        PostDetailLikeResponse likeInfo
+) {
+
+    public static PostDetailResponse of(String loginUserId, String userName, Post post, List<String> urlList) {
+        boolean likeStatus = !Objects.isNull(loginUserId) && post.isLikeContain(loginUserId);
+        return new PostDetailResponse(
+                userName,
+                post.getScore(),
+                post.getContents(),
+                urlList,
+                new PostDetailLikeResponse(post.likeCount(), likeStatus)
+        );
     }
 }
