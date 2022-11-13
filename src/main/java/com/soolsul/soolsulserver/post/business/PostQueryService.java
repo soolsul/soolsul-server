4f81d7b85bc8308de0532595fe3a08d5cc64316f
jiwoo <zbqmgldjfh@gmail.com>
2022-11-13 21:03:15 +0900

    feat: PostList 반환 기능 구현 완료
    아직 BarRepository에서 반경으로 필터링 하는 부분은 미구현

--- src/main/java/com/soolsul/soolsulserver/post/business/PostQueryService.java
+++ src/main/java/com/soolsul/soolsulserver/post/business/PostQueryService.java
@@ -1,11 +1,19 @@
 package com.soolsul.soolsulserver.post.business;
 
+import com.soolsul.soolsulserver.bar.businees.dto.BarLookupResponse;
+import com.soolsul.soolsulserver.bar.persistence.BarQueryRepository;
+import com.soolsul.soolsulserver.common.userlocation.UserLocation;
+import com.soolsul.soolsulserver.common.userlocation.UserLocationBasedSquareRange;
 import com.soolsul.soolsulserver.post.domain.Post;
 import com.soolsul.soolsulserver.post.domain.PostPhoto;
 import com.soolsul.soolsulserver.post.domain.PostRepository;
 import com.soolsul.soolsulserver.post.exception.PostNotFoundException;
 import com.soolsul.soolsulserver.post.presentation.dto.PostDetailResponse;
+import com.soolsul.soolsulserver.post.presentation.dto.PostListResponse;
+import com.soolsul.soolsulserver.post.presentation.dto.UserLocationRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -18,6 +26,7 @@ import java.util.stream.Collectors;
 public class PostQueryService {
 
     private final PostRepository postRepository;
+    private final BarQueryRepository barRepository;
 
     public PostDetailResponse findPostDetail(String loginUserId, String postId) {
         Post findPost = postRepository.findById(postId)
@@ -34,6 +43,32 @@ public class PostQueryService {
         return findPost.getPhotos()
                 .stream()
                 .map(PostPhoto::getUrl)
+                .collect(Collectors.toList());
+    }
+
+    public PostListResponse findAllPostByLocation(String loginUserId, UserLocationRequest locationRequest, Pageable pageable) {
+        UserLocation userLocation = UserLocation.from(locationRequest);
+        UserLocationBasedSquareRange squareRange = new UserLocationBasedSquareRange(userLocation);
+
+        // TODO : squareRange기반으로 bar목록을 찾아와야함, 아직 미구현 상태라 틀만 잡음
+
+        List<BarLookupResponse> findBars = barRepository.findBarMeetingConditions(null);
+        List<String> barIds = extractBarIds(findBars);
+
+        Slice<Post> postListByLocation = postRepository.findPostListByLocation(barIds, pageable);
+
+        return new PostListResponse(lookUpPostDetails(loginUserId, postListByLocation));
+    }
+
+    private List<PostDetailResponse> lookUpPostDetails(String loginUserId, Slice<Post> postListByLocation) {
+        return postListByLocation.stream()
+                .map(post -> PostDetailResponse.of(loginUserId, "tempUserName", post, convertImageUrlList(post)))
+                .collect(Collectors.toList());
+    }
+
+    private List<String> extractBarIds(List<BarLookupResponse> findBars) {
+        return findBars.stream()
+                .map(bar -> bar.barId())
                 .collect(Collectors.toList());
     }
 }
