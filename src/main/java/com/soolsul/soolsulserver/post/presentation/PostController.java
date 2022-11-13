c7a48c94e26c453629a543581187c8ff65c895bf
jiwoo <zbqmgldjfh@gmail.com>
2022-11-14 08:42:06 +0900

    refactor: Query, Command 컨트롤러 통합, Service Gateway에서 CQRS적용

--- src/main/java/com/soolsul/soolsulserver/post/presentation/PostController.java
+++ src/main/java/com/soolsul/soolsulserver/post/presentation/PostController.java
@@ -4,6 +4,7 @@ import com.soolsul.soolsulserver.auth.User;
 import com.soolsul.soolsulserver.common.response.BaseResponse;
 import com.soolsul.soolsulserver.common.response.ResponseCodeAndMessages;
 import com.soolsul.soolsulserver.post.business.PostServiceGateway;
+import com.soolsul.soolsulserver.post.presentation.dto.PostCreateRequest;
 import com.soolsul.soolsulserver.post.presentation.dto.PostDetailResponse;
 import com.soolsul.soolsulserver.post.presentation.dto.PostListResponse;
 import com.soolsul.soolsulserver.post.presentation.dto.UserLocationRequest;
@@ -14,27 +15,38 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/posts")
-public class PostQueryController {
+public class PostController {
 
     private final PostServiceGateway postServiceGateway;
+
+    @PostMapping
+    public ResponseEntity<BaseResponse<Void>> createPost(@Valid @RequestBody PostCreateRequest request, Authentication authentication) {
+        User user = (User) authentication.getPrincipal();
+        postServiceGateway.create(user.getId(), request);
+        return new ResponseEntity<>(new BaseResponse<>(ResponseCodeAndMessages.FEED_CREATE_SUCCESS, null), HttpStatus.OK);
+    }
 
     @GetMapping("/{postId}")
     public ResponseEntity<BaseResponse<PostDetailResponse>> findDetailPost(@PathVariable String postId, Authentication authentication) {
         User user = (User) authentication.getPrincipal();
         PostDetailResponse postDetailResponse = postServiceGateway.find(user.getId(), postId);
-        return new ResponseEntity<>(new BaseResponse<>(ResponseCodeAndMessages.FEED_FIND_SUCCESS, postDetailResponse), HttpStatus.OK);
+        return ResponseEntity.ok(new BaseResponse<>(ResponseCodeAndMessages.FEED_FIND_SUCCESS, postDetailResponse));
     }
 
     @GetMapping
     public ResponseEntity<BaseResponse<PostListResponse>> findAllDetailPost(UserLocationRequest locationRequest, Pageable pageable, Authentication authentication) {
         User user = (User) authentication.getPrincipal();
         PostListResponse postListResponse = postServiceGateway.findAll(user.getId(), locationRequest, pageable);
-        return new ResponseEntity<>(new BaseResponse<>(ResponseCodeAndMessages.FEED_FIND_ALL_SUCCESS, postListResponse), HttpStatus.OK);
+        return ResponseEntity.ok(new BaseResponse<>(ResponseCodeAndMessages.FEED_FIND_ALL_SUCCESS, postListResponse));
     }
 }
