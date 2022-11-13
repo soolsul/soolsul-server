f562cd96d8f849df779410469a159c86b6f3e6a5
jiwoo <zbqmgldjfh@gmail.com>
2022-11-14 08:42:06 +0900

    refactor: Query, Command 컨트롤러 통합, Service Gateway에서 CQRS적용

--- src/main/java/com/soolsul/soolsulserver/post/business/PostQueryService.java
+++ src/main/java/com/soolsul/soolsulserver/post/business/PostQueryService.java
@@ -4,6 +4,9 @@ import com.soolsul.soolsulserver.bar.businees.dto.BarLookupResponse;
 import com.soolsul.soolsulserver.bar.persistence.BarQueryRepository;
 import com.soolsul.soolsulserver.common.userlocation.UserLocation;
 import com.soolsul.soolsulserver.common.userlocation.UserLocationBasedSquareRange;
+import com.soolsul.soolsulserver.post.business.dto.PostDetailLikeResponse;
+import com.soolsul.soolsulserver.post.business.dto.PostDetailStoreResponse;
+import com.soolsul.soolsulserver.post.business.dto.PostDetailUserResponse;
 import com.soolsul.soolsulserver.post.domain.Post;
 import com.soolsul.soolsulserver.post.domain.PostPhoto;
 import com.soolsul.soolsulserver.post.domain.PostRepository;
@@ -18,6 +21,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 @Service
@@ -32,18 +36,21 @@ public class PostQueryService {
         Post findPost = postRepository.findById(postId)
                 .orElseThrow(PostNotFoundException::new);
         // TODO : userDetailService에서 Post의 주인을 찾아와야함
-        // TODO : Bar 정보 찾아와야 함, 아직 Bar 도메인 미구현
+        // TODO : Bar 정보 찾아와야 함, 아직 Bar 도메인 미구현 단건 Bar
 
         List<String> urlList = convertImageUrlList(findPost);
 
-        return PostDetailResponse.of(loginUserId, "tempUserName", findPost, urlList);
-    }
+        boolean userClickedLike = isLoginUserClickedLike(loginUserId, findPost);
 
-    private List<String> convertImageUrlList(Post findPost) {
-        return findPost.getPhotos()
-                .stream()
-                .map(PostPhoto::getUrl)
-                .collect(Collectors.toList());
+        return new PostDetailResponse(
+                "tempUserName",
+                findPost.getScore(),
+                findPost.getContents(),
+                urlList,
+                new PostDetailLikeResponse(findPost.likeCount(), userClickedLike),
+                new PostDetailUserResponse("tempPostOwnerId", "tempPostOwnerName", "url"),
+                new PostDetailStoreResponse()
+        );
     }
 
     public PostListResponse findAllPostByLocation(String loginUserId, UserLocationRequest locationRequest, Pageable pageable) {
@@ -60,9 +67,33 @@ public class PostQueryService {
         return new PostListResponse(lookUpPostDetails(loginUserId, postListByLocation));
     }
 
+    private boolean isLoginUserClickedLike(String loginUserId, Post findPost) {
+        return !Objects.isNull(loginUserId) && findPost.isLikeContain(loginUserId);
+    }
+
+    private List<String> convertImageUrlList(Post findPost) {
+        return findPost.getPhotos()
+                .stream()
+                .map(PostPhoto::getUrl)
+                .collect(Collectors.toList());
+    }
+
     private List<PostDetailResponse> lookUpPostDetails(String loginUserId, Slice<Post> postListByLocation) {
         return postListByLocation.stream()
-                .map(post -> PostDetailResponse.of(loginUserId, "tempUserName", post, convertImageUrlList(post)))
+                .map(post -> {
+                    boolean userClickedLike = isLoginUserClickedLike(loginUserId, post);
+                    List<String> urlList = convertImageUrlList(post);
+
+                    return new PostDetailResponse(
+                            "tempUserName",
+                            post.getScore(),
+                            post.getContents(),
+                            urlList,
+                            new PostDetailLikeResponse(post.likeCount(), userClickedLike),
+                            new PostDetailUserResponse("tempPostOwnerId", "tempPostOwnerName", "url"),
+                            new PostDetailStoreResponse()
+                    );
+                })
                 .collect(Collectors.toList());
     }
 
