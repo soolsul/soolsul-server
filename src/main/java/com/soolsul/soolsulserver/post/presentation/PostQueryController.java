a81e693d10fb4fb12f8a7ab532491f9f6bdd8c42
jiwoo <zbqmgldjfh@gmail.com>
2022-11-13 21:03:15 +0900

    feat: PostList 반환 기능 구현 완료
    아직 BarRepository에서 반경으로 필터링 하는 부분은 미구현

--- src/main/java/com/soolsul/soolsulserver/post/presentation/PostQueryController.java
+++ src/main/java/com/soolsul/soolsulserver/post/presentation/PostQueryController.java
@@ -5,7 +5,10 @@ import com.soolsul.soolsulserver.common.response.BaseResponse;
 import com.soolsul.soolsulserver.common.response.ResponseCodeAndMessages;
 import com.soolsul.soolsulserver.post.business.PostServiceGateway;
 import com.soolsul.soolsulserver.post.presentation.dto.PostDetailResponse;
+import com.soolsul.soolsulserver.post.presentation.dto.PostListResponse;
+import com.soolsul.soolsulserver.post.presentation.dto.UserLocationRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
@@ -26,5 +29,12 @@ public class PostQueryController {
         User user = (User) authentication.getPrincipal();
         PostDetailResponse postDetailResponse = postServiceGateway.find(user.getId(), postId);
         return new ResponseEntity<>(new BaseResponse<>(ResponseCodeAndMessages.FEED_FIND_SUCCESS, postDetailResponse), HttpStatus.OK);
+    }
+
+    @GetMapping
+    public ResponseEntity<BaseResponse<PostListResponse>> findAllDetailPost(UserLocationRequest locationRequest, Pageable pageable, Authentication authentication) {
+        User user = (User) authentication.getPrincipal();
+        PostListResponse postListResponse = postServiceGateway.findAll(user.getId(), locationRequest, pageable);
+        return new ResponseEntity<>(new BaseResponse<>(ResponseCodeAndMessages.FEED_FIND_ALL_SUCCESS, postListResponse), HttpStatus.OK);
     }
 }
