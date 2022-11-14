ab5a7ac8cde87c8e2cfae4a850fbb5a082fbac5a
cooper <pbg0205@naver.com>
2022-11-14 17:47:43 +0900

    test: 단일 바 조회 테스트 코드 추가

---
+++ src/test/java/com/soolsul/soolsulserver/bar/persistence/BarQueryRepositoryTest.java
@@ -0,0 +1,50 @@
+package com.soolsul.soolsulserver.bar.persistence;
+
+import com.soolsul.soolsulserver.bar.domain.Bar;
+import com.soolsul.soolsulserver.bar.exception.BarNotFoundException;
+import com.soolsul.soolsulserver.bar.presentation.dto.BarLookupResponse;
+import com.soolsul.soolsulserver.config.QueryDslConfig;
+import com.soolsul.soolsulserver.region.domain.Location;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.context.annotation.Import;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+@Import(QueryDslConfig.class)
+@DataJpaTest(
+        includeFilters = @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = BarQueryRepository.class)
+)
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+class BarQueryRepositoryTest {
+
+    @Autowired
+    private BarQueryRepository barQueryRepository;
+
+    @Autowired
+    private TestEntityManager testEntityManager;
+
+    @DisplayName("알콜 카테고리 아이디를 통해 바-술종류 태그를 조회한다")
+    @Test
+    void find_by_id() {
+        Bar bar = testEntityManager.persist(
+                new Bar("regionId01", "barCategoryId", "barName","description", new Location(50.0, 50.0))
+        );
+
+        BarLookupResponse findBar = barQueryRepository.findById(bar.getId()).orElseThrow(BarNotFoundException::new);
+
+        assertAll(
+                () -> assertThat(bar.getId()).isEqualTo(findBar.id()),
+                () -> assertThat(bar.getDescription()).isEqualTo(findBar.description()),
+                () -> assertThat(bar.getBarCategoryId()).isEqualTo(findBar.barCategoryId())
+
+        );
+    }
+}
