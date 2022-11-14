0ae7985ebb44058d6b9cacfca4e87fcc35b0a906
jiwoo <zbqmgldjfh@gmail.com>
2022-11-14 15:10:03 +0900

    feat: FirstLoginAuthenticationFilter 구현
    맨 처음 email, password를 통한 로그인 필터 구현

---
+++ src/main/java/com/soolsul/soolsulserver/auth/filter/FirstLoginAuthenticationFilter.java
@@ -0,0 +1,48 @@
+package com.soolsul.soolsulserver.auth.filter;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.soolsul.soolsulserver.auth.dto.UserDto;
+import com.soolsul.soolsulserver.auth.token.FirstLoginAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.util.StringUtils;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class FirstLoginAuthenticationFilter extends AbstractAuthenticationProcessingFilter {
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    public FirstLoginAuthenticationFilter() {
+        super(new AntPathRequestMatcher("/api/login"));
+    }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException, IOException, ServletException {
+        if (!isJsonLogin(request)) {
+            throw new IllegalStateException("Authentication is not supportes");
+        }
+
+        UserDto userDto = objectMapper.readValue(request.getReader(), UserDto.class);
+        if (StringUtils.isEmpty(userDto.getEmail()) || StringUtils.isEmpty(userDto.getPassword())) {
+            throw new IllegalArgumentException("UserId is empty");
+        }
+
+        FirstLoginAuthenticationToken authenticationToken = new FirstLoginAuthenticationToken(userDto.getEmail(), userDto.getPassword());
+
+        return getAuthenticationManager().authenticate(authenticationToken);
+    }
+
+    private boolean isJsonLogin(HttpServletRequest request) {
+        if ("JSONLoginHttpRequest".equals(request.getHeader("X-Requested-With"))) {
+            return true;
+        }
+
+        return false;
+    }
+}
---
+++ src/main/java/com/soolsul/soolsulserver/auth/token/FirstLoginAuthenticationToken.java
@@ -0,0 +1,38 @@
+package com.soolsul.soolsulserver.auth.token;
+
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+
+import java.util.Collection;
+
+public class FirstLoginAuthenticationToken extends AbstractAuthenticationToken {
+
+    private final Object principal;
+    private Object credentials;
+
+    // 인증 이전에 사용
+    public FirstLoginAuthenticationToken(Object principal, Object credentials) {
+        super(null);
+        this.principal = principal;
+        this.credentials = credentials;
+        setAuthenticated(false);
+    }
+
+    // 인증 이후 사용
+    public FirstLoginAuthenticationToken(Object principal, Object credentials, Collection<? extends GrantedAuthority> authorities) {
+        super(authorities);
+        this.principal = principal;
+        this.credentials = credentials;
+        super.setAuthenticated(true);
+    }
+
+    @Override
+    public Object getCredentials() {
+        return this.credentials;
+    }
+
+    @Override
+    public Object getPrincipal() {
+        return this.principal;
+    }
+}
