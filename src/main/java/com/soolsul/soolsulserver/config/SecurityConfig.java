e9c1ef8c153bfff092b3d9e6adba448ba9f790cc
cooper <pbg0205@naver.com>
2022-11-18 23:29:43 +0900

    fix: security cors addAllowedOriginPattern 변경

--- src/main/java/com/soolsul/soolsulserver/config/SecurityConfig.java
+++ src/main/java/com/soolsul/soolsulserver/config/SecurityConfig.java
@@ -99,7 +99,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.addAllowedOrigin("*");
+        configuration.addAllowedOriginPattern("*");
         configuration.addAllowedHeader("*");
         configuration.addAllowedMethod("*");
         configuration.setAllowCredentials(true);
