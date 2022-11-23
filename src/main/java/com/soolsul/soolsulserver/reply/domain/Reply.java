a02390840e22596dc72dfd013396bebeba3e3532
jiwoo <zbqmgldjfh@gmail.com>
2022-11-23 15:38:47 +0900

    refactor: Reply에서 content를 Embedded방식으로 변경

--- src/main/java/com/soolsul/soolsulserver/reply/domain/Reply.java
+++ src/main/java/com/soolsul/soolsulserver/reply/domain/Reply.java
@@ -5,13 +5,21 @@ import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import java.util.Objects;
 
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@SQLDelete(sql = "update reply set deleted = true where id = ?")
+@Where(clause = "deleted = false")
 public class Reply extends BaseTimeEntity {
 
     @Id
@@ -27,15 +35,16 @@ public class Reply extends BaseTimeEntity {
 
     private String responseId;
 
-    @Lob
-    @Column
-    private String contents;
+    @Embedded
+    private ReplyContent contents;
+
+    private Boolean deleted = Boolean.FALSE;
 
     public Reply(String ownerId, String postId, String responseId, String contents) {
         this.ownerId = ownerId;
         this.postId = postId;
         this.responseId = responseId;
-        this.contents = contents;
+        this.contents = new ReplyContent(contents);
     }
 
     @Override
