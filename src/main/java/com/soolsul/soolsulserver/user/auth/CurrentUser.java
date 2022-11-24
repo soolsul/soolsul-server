ed45ce5a7cea9949ac4033d879d9f07a62cd6e48
cooper <pbg0205@naver.com>
2022-11-24 13:10:56 +0900

    refactor: Authentication 제거 리팩토링

---
+++ src/main/java/com/soolsul/soolsulserver/user/auth/CurrentUser.java
@@ -0,0 +1,14 @@
+package com.soolsul.soolsulserver.user.auth;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.PARAMETER)
+@Retention(RetentionPolicy.RUNTIME)
+@AuthenticationPrincipal
+public @interface CurrentUser {
+}
