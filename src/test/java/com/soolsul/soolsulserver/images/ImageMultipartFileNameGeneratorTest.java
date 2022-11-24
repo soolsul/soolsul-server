c83fcb1f2cc76642cba77f613e57a8337d610216
cooper <pbg0205@naver.com>
2022-11-25 08:32:54 +0900

    test: ImageMultipartFileNameGeneratorTest 테스트

---
+++ src/test/java/com/soolsul/soolsulserver/images/ImageMultipartFileNameGeneratorTest.java
@@ -0,0 +1,43 @@
+package com.soolsul.soolsulserver.images;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockMultipartFile;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+class ImageMultipartFileNameGeneratorTest {
+
+    private ImageMultipartFileNameGenerator imageMultipartFileNameGenerator;
+
+    @BeforeEach
+    public void init() {
+        this.imageMultipartFileNameGenerator = new ImageMultipartFileNameGenerator();
+    }
+
+    @DisplayName("multipartfile 에서 파일 이름을 추출한다")
+    @Test
+    void generateFileName() {
+        //given
+        String multipartName = "multipartFile";
+
+        String imageName = "imageName";
+        String imageFileExtension = ".png";
+        String imageFileName = imageName + imageFileExtension;
+
+        MockMultipartFile multipartFile = new MockMultipartFile(
+                multipartName,
+                imageFileName,
+                MediaType.MULTIPART_FORM_DATA_VALUE,
+                "image".getBytes());
+
+        //when
+        String generatedFileName = imageMultipartFileNameGenerator.generateFileName(multipartFile);
+
+        //then
+        assertThat(generatedFileName).startsWith(imageName);
+        System.out.println(generatedFileName);
+    }
+}
