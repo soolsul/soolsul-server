8735572a2c992f0081ec1fd2c48eee2359d5ecc3
jiwoo <zbqmgldjfh@gmail.com>
2022-12-24 15:21:21 +0900

    docs: Post 전체 조회 문서화 완료

--- src/test/java/com/soolsul/soolsulserver/documentation/PostDocumentation.java
+++ src/test/java/com/soolsul/soolsulserver/documentation/PostDocumentation.java
@@ -5,6 +5,7 @@ import com.soolsul.soolsulserver.post.common.dto.response.PostDetailLikeResponse
 import com.soolsul.soolsulserver.post.common.dto.response.PostDetailResponse;
 import com.soolsul.soolsulserver.post.common.dto.response.PostDetailStoreResponse;
 import com.soolsul.soolsulserver.post.common.dto.response.PostDetailUserResponse;
+import com.soolsul.soolsulserver.post.common.dto.response.PostListResponse;
 import com.soolsul.soolsulserver.post.facade.PostFacadeGateway;
 import com.soolsul.soolsulserver.post.presentation.PostController;
 import org.junit.jupiter.api.DisplayName;
@@ -38,6 +39,7 @@ import static org.springframework.restdocs.payload.PayloadDocumentation.requestF
 import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
 import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
 import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
+import static org.springframework.restdocs.request.RequestDocumentation.requestParameters;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -105,6 +107,82 @@ public class PostDocumentation extends Documentation {
                 );
     }
 
+    @DisplayName("문서화 : Post 목록 조회")
+    @WithMockUser
+    @Test
+    void find_post_list_success() throws Exception {
+        List<String> imagesUrl = List.of("url1", "url2", "url3");
+        PostDetailResponse postDetailResponse1 = new PostDetailResponse(
+                "post_id_1", 4.3f, "contents1", imagesUrl,
+                new PostDetailLikeResponse(42, true),
+                new PostDetailUserResponse("userId", "nick name", "profile_url"),
+                new PostDetailStoreResponse("store_id", "name", "this is store")
+        );
+
+        PostDetailResponse postDetailResponse2 = new PostDetailResponse(
+                "post_id_2", 3.4f, "contents2", imagesUrl,
+                new PostDetailLikeResponse(42, true),
+                new PostDetailUserResponse("userId", "nick name", "profile_url"),
+                new PostDetailStoreResponse("store_id", "name", "this is store")
+        );
+
+        PostListResponse postListResponse = new PostListResponse(List.of(postDetailResponse1, postDetailResponse2));
+
+        given(postFacadeGateway.findAll(any(), any(), any())).willReturn(postListResponse);
+
+        mockMvc.perform(RestDocumentationRequestBuilders.get("/api/posts")
+                        .param("latitude", String.valueOf(37.565494))
+                        .param("longitude", String.valueOf(126.992493))
+                        .param("level", String.valueOf(7))
+                        .param("page", String.valueOf(0))
+                        .accept(MediaTypes.APPLICATION_JSON)
+                        .header("Authorization", "bearer login-jwt-token"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andDo(
+                        document("find-all-post",
+                                preprocessRequest(prettyPrint()),
+                                preprocessResponse(prettyPrint()),
+                                findAllPostRequestParam(),
+                                findAllPostResponseBody())
+                );
+    }
+
+    private Snippet findAllPostRequestParam() {
+        return requestParameters(
+                parameterWithName("longitude").description("위도"),
+                parameterWithName("latitude").description("경도"),
+                parameterWithName("level").description("지도 레밸"),
+                parameterWithName("page").description("요청 페이지")
+        );
+    }
+
+    private Snippet findAllPostResponseBody() {
+        return responseFields(
+                fieldWithPath("code").type(JsonFieldType.STRING).description(Constants.RESPONSE_ID),
+                fieldWithPath("message").type(JsonFieldType.STRING).description(Constants.RESPONSE_MESSAGE),
+                fieldWithPath("data").type(JsonFieldType.OBJECT).description(Constants.RESPONSE_DATA).optional(),
+                fieldWithPath("data.postList[].postId").type(JsonFieldType.STRING).description("피드 ID"),
+                fieldWithPath("data.postList[].score").type(JsonFieldType.NUMBER).description("피드에 남긴 가게 평점"),
+                fieldWithPath("data.postList[].contents").type(JsonFieldType.STRING).description("피드 본문"),
+                fieldWithPath("data.postList[].imageUrls").type(JsonFieldType.ARRAY).description("피드에 게시된 사진들"),
+                fieldWithPath("data.postList[].like.count").type(JsonFieldType.NUMBER).description("피드 좋아요 수"),
+                fieldWithPath("data.postList[].like.userLikeStatus").type(JsonFieldType.BOOLEAN).description("피드에 사용자가 좋아요를 눌렀는지"),
+                fieldWithPath("data.postList[].user.userId").type(JsonFieldType.STRING).description("피드 작성자 ID"),
+                fieldWithPath("data.postList[].user.userNickname").type(JsonFieldType.STRING).description("피드 작성자 별칭"),
+                fieldWithPath("data.postList[].user.userProfileUrl").type(JsonFieldType.STRING).description("피드 작성자 이미지 주소"),
+                fieldWithPath("data.postList[].store.storeId").type(JsonFieldType.STRING).description("가게 ID"),
+                fieldWithPath("data.postList[].store.storeName").type(JsonFieldType.STRING).description("가게 이름"),
+                fieldWithPath("data.postList[].store.description").type(JsonFieldType.STRING).description("가게 설명")
+        );
+    }
+
+    private Snippet findPostRequestParam() {
+        return pathParameters(
+                parameterWithName("postId").description("리뷰 ID")
+        );
+    }
+
     private Snippet findPostResponseBody() {
         return responseFields(
                 fieldWithPath("code").type(JsonFieldType.STRING).description(Constants.RESPONSE_ID),
@@ -123,12 +201,6 @@ public class PostDocumentation extends Documentation {
                 fieldWithPath("data.store.storeName").type(JsonFieldType.STRING).description("가게 이름"),
                 fieldWithPath("data.store.description").type(JsonFieldType.STRING).description("가게 설명")
 
-        );
-    }
-
-    private Snippet findPostRequestParam() {
-        return pathParameters(
-                parameterWithName("postId").description("리뷰 ID")
         );
     }
 
