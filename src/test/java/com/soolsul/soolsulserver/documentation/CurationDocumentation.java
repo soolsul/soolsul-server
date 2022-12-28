16ca42e7fd8b1bf6a1e0d53261ec5bfb1c6f6ab0
jiwoo <zbqmgldjfh@gmail.com>
2022-12-28 14:14:34 +0900

    docs: Curation 단건 조회 문서화

--- src/test/java/com/soolsul/soolsulserver/documentation/CurationDocumentation.java
+++ src/test/java/com/soolsul/soolsulserver/documentation/CurationDocumentation.java
@@ -1,7 +1,13 @@
 package com.soolsul.soolsulserver.documentation;
 
+import com.soolsul.soolsulserver.bar.common.dto.response.BarSnackMenuResponse;
+import com.soolsul.soolsulserver.bar.common.dto.response.BarStreetNameAddressResponse;
+import com.soolsul.soolsulserver.curation.common.dto.response.BarOpeningHoursResponse;
+import com.soolsul.soolsulserver.curation.common.dto.response.CurationDetailLookupResponse;
 import com.soolsul.soolsulserver.curation.common.dto.response.CurationListLookupResponse;
+import com.soolsul.soolsulserver.curation.common.dto.response.CurationPostLookupResponse;
 import com.soolsul.soolsulserver.curation.common.dto.response.CurationsLookupResponse;
+import com.soolsul.soolsulserver.curation.common.dto.response.PostPhotoImageResponse;
 import com.soolsul.soolsulserver.curation.facade.CurationQueryFacade;
 import com.soolsul.soolsulserver.curation.presentation.CurationQueryController;
 import org.junit.jupiter.api.DisplayName;
@@ -16,8 +22,8 @@ import org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders;
 import org.springframework.restdocs.payload.JsonFieldType;
 import org.springframework.restdocs.snippet.Snippet;
 import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
-import org.springframework.security.test.context.support.WithMockUser;
 
+import java.time.LocalTime;
 import java.util.List;
 
 import static com.soolsul.soolsulserver.documentation.Constants.MAP_LEVEL_DESCRIPTION;
@@ -29,6 +35,7 @@ import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.docu
 import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
 import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
 import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
 import static org.springframework.restdocs.request.RequestDocumentation.requestParameters;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -41,7 +48,6 @@ public class CurationDocumentation extends Documentation {
     CurationQueryFacade curationQueryFacade;
 
     @DisplayName("문서화 : Curation 목록 조회")
-    @WithMockUser
     @Test
     void find_curation_list_success() throws Exception {
         CurationListLookupResponse curationResponseOne = new CurationListLookupResponse("curation_id_1", "url", "title_1", "content_1", "감성있는1", "테그1");
@@ -62,6 +68,61 @@ public class CurationDocumentation extends Documentation {
                                 findAllCurationRequestParam(),
                                 findAllCurationResponseBody())
                 );
+    }
+
+    @DisplayName("문서화 : Curation 단건 조회")
+    @Test
+    void find_specific_curation_success() throws Exception {
+        BarSnackMenuResponse snackMenuResponse = new BarSnackMenuResponse("스파게티", 15000L);
+        CurationPostLookupResponse curationResponse = new CurationPostLookupResponse("contents", "사용자1", List.of(new PostPhotoImageResponse("url1"), new PostPhotoImageResponse("url2")), 12);
+
+        LocalTime start = LocalTime.of(16, 00, 00);
+        LocalTime end = LocalTime.of(23, 59, 00);
+        CurationDetailLookupResponse curationDetailLookupResponse = new CurationDetailLookupResponse("curation_title", "contents", "010-1234-5678",
+                new BarStreetNameAddressResponse("region_name", "seoul", "동작구", "양녕로", 133, "road_detail", "location_detail"),
+                new BarOpeningHoursResponse(start, end),
+                List.of(snackMenuResponse),
+                List.of(curationResponse)
+        );
+
+        given(curationQueryFacade.findCurationDetailsByCurationId(any())).willReturn(curationDetailLookupResponse);
+
+        mockMvc.perform(RestDocumentationRequestBuilders.get("/api/curations/{curationId}", "curation_uuid")
+                        .header("Authorization", "bearer login-jwt-token")
+                        .accept(MediaTypes.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andDo(
+                        document("find-specific-curation",
+                                findSpecificCurationRequestParam(),
+                                findSpecificCurationResponseBody())
+                );
+    }
+
+    private Snippet findSpecificCurationRequestParam() {
+        return pathParameters(
+                parameterWithName("curationId").description("큐레이션 ID")
+        );
+    }
+
+    private Snippet findSpecificCurationResponseBody() {
+        return responseFields(
+                fieldWithPath("code").type(JsonFieldType.STRING).description(Constants.RESPONSE_ID),
+                fieldWithPath("message").type(JsonFieldType.STRING).description(Constants.RESPONSE_MESSAGE),
+                fieldWithPath("data").type(JsonFieldType.OBJECT).description(Constants.RESPONSE_DATA).optional(),
+                fieldWithPath("data.curationTitle").type(JsonFieldType.STRING).description("큐레이션 ID"),
+                fieldWithPath("data.curationContent").type(JsonFieldType.STRING).description("대표 가게 사진"),
+                fieldWithPath("data.phoneNumber").type(JsonFieldType.STRING).description("가게 연락처"),
+                fieldWithPath("data.barStreetNameAddressResponse.simpleStreetNameAddress").type(JsonFieldType.STRING).description("간단 거리 주소"),
+                fieldWithPath("data.barStreetNameAddressResponse.detailStreetNameAddress").type(JsonFieldType.STRING).description("상세 거리 주소"),
+                fieldWithPath("data.barOpeningHoursResponse.openTime").type(JsonFieldType.STRING).description("가게 여는 시간"),
+                fieldWithPath("data.barOpeningHoursResponse.closeTime").type(JsonFieldType.STRING).description("가게 닫는 시간"),
+                fieldWithPath("data.barSnackMenuResponses[].snackMenuName").type(JsonFieldType.STRING).description("매뉴 이름"),
+                fieldWithPath("data.barSnackMenuResponses[].cost").type(JsonFieldType.NUMBER).description("매뉴 가격"),
+                fieldWithPath("data.curationPostLookupResponses[].content").type(JsonFieldType.STRING).description("내용"),
+                fieldWithPath("data.curationPostLookupResponses[].userName").type(JsonFieldType.STRING).description("작성자 이름"),
+                fieldWithPath("data.curationPostLookupResponses[].postImageUrls[].postPhotoImageUrl").type(JsonFieldType.STRING).description("사진 url"),
+                fieldWithPath("data.curationPostLookupResponses[].userLikes").type(JsonFieldType.NUMBER).description("좋아요 수")
+        );
     }
 
     private Snippet findAllCurationRequestParam() {
