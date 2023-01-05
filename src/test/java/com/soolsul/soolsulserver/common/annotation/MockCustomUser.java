4e14769701dfb134ca3b661e8b74e63767667147
jiwoo <zbqmgldjfh@gmail.com>
2023-01-05 17:20:43 +0900

    feat(MockCustomUser): MockCustomUser에서 명시적 setupBefore() 오버라이드 제거

--- src/test/java/com/soolsul/soolsulserver/common/annotation/MockCustomUser.java
+++ src/test/java/com/soolsul/soolsulserver/common/annotation/MockCustomUser.java
@@ -1,8 +1,6 @@
 package com.soolsul.soolsulserver.common.annotation;
 
 import com.soolsul.soolsulserver.common.factory.MockCustomUserSecurityContextFactory;
-import org.springframework.core.annotation.AliasFor;
-import org.springframework.security.test.context.support.TestExecutionEvent;
 import org.springframework.security.test.context.support.WithSecurityContext;
 
 import java.lang.annotation.ElementType;
@@ -22,7 +20,4 @@ public @interface MockCustomUser {
     String[] roles() default {"USER"};
 
     String password() default "password";
-
-    @AliasFor(annotation = WithSecurityContext.class)
-    TestExecutionEvent setupBefore() default TestExecutionEvent.TEST_METHOD;
 }
