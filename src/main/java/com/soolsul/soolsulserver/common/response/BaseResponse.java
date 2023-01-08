daa0dc601499f5c133d47c92b808811cf3bd3ae2
cooper <pbg0205@naver.com>
2023-01-08 21:14:04 +0900

    refactor: BaseResponse record 타입 변경

--- src/main/java/com/soolsul/soolsulserver/common/response/BaseResponse.java
+++ src/main/java/com/soolsul/soolsulserver/common/response/BaseResponse.java
@@ -1,10 +1,5 @@
 package com.soolsul.soolsulserver.common.response;
 
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-
-@Getter
-@RequiredArgsConstructor
 public record BaseResponse<T>(String code, String message, T data) {
 
 }
